d2e4aab126097a401fbeba3769af7155ce88cb0e
davideiacobs@gmail.com <davideiacobs@gmail.com>
2017-05-25 17:40:30 +0200

    template + header-footer

---
+++ SocialDevelop/src/java/socialdevelop/controller/Home.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socialdevelop.controller;
+
+import it.univaq.f4i.iw.framework.data.DataLayerException;
+import it.univaq.f4i.iw.framework.result.TemplateManagerException;
+import it.univaq.f4i.iw.framework.result.TemplateResult;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author iacobs
+ */
+public class Home extends SocialDevelopBaseController {
+    
+    
+    private void action_home(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+                
+           RequestDispatcher view = request.getRequestDispatcher("templates/home.html");
+            view.forward(request, response);
+            //TemplateResult res = new TemplateResult(getServletContext());
+            //aggiungiamo al template un wrapper che ci permette di chiamare la funzione stripSlashes
+            //add to the template a wrapper object that allows to call the stripslashes function
+            //res.activate("home.html",request, response);  //al posto di ciao va inserito il nome dell'html da attivare
+       
+    }
+    
+    
+    
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException{
+        
+        try {
+            action_home(request, response);
+        } catch (IOException ex) {
+            Logger.getLogger(MakeLoginReg.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TemplateManagerException ex) {
+            Logger.getLogger(MakeLoginReg.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
