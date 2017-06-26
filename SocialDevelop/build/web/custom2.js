06f6a782d7934b4ae593e38bf3834c67736c472b
davideiacobs@gmail.com <davideiacobs@gmail.com>
2017-06-26 16:50:20 +0200

    update select skills

--- SocialDevelop/build/web/custom2.js
+++ SocialDevelop/build/web/custom2.js
@@ -5,13 +5,10 @@ $( "#submitsearch" ).click(function() {
 function readURL(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
-
             reader.onload = function (e) {
                 $('#img-profilo')
-                    .attr('src', e.target.result);
-                    
+                    .attr('src', e.target.result);                    
             };
-
             reader.readAsDataURL(input.files[0]);
         }
 }
@@ -42,6 +39,10 @@ $(".add-skill-btn").on("click",function(){
     var level = $("#select-level option:selected").val();
     var skill_level = skill.val()+":"+level;
     var userid = $("#userid").val();
+    var skill_text = skill.text();
+    if(skill_text.indexOf("-")>=0){
+        skill_text = skill_text.split("-")[1];
+    }
     $.ajax({
           datatype: 'text/plain',
           type: 'post',
@@ -52,7 +53,9 @@ $(".add-skill-btn").on("click",function(){
           },
           success: function (response) {
               if(response == 1){
-              $(".list-skill").prepend("<li><a class='skill-name' >"+skill.text()+"&nbsp;</a><a id='skill-level'>"+level+"</a>\n\
+                 
+                  
+              $(".list-skill").prepend("<li><a class='skill-name' >"+skill_text+"&nbsp;</a><a id='skill-level'>"+level+"</a>\n\
             <span><button onclick='remove_skill(this)' type='button' id='rm-skill-btn' class='btn btn-default btn-sm remove-skill-button rm-skill-btn'><span class='glyphicon glyphicon-remove'></span></button></span></li>");   
             }
          }
